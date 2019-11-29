7d99fbb98d7b372fb9d680efab1fee76b1d39ee4
Praveen Bisht <prvnbist@gmail.com>
2019-11-29 16:26:41 +0530

    feat(theme): custom config

--- doczrc.js
+++ doczrc.js
@@ -3,5 +3,16 @@ export default {
   title: "DailyGit Docs",
   description: "Documentation for dailygit",
   public: "/public",
-  port: 3001
+  port: 3001,
+  themeConfig: {
+    colors: {
+      header: {
+        bg: "#8AC03B",
+        button: {
+          bg: "#6b9a25",
+          color: "#fff"
+        }
+      }
+    }
+  }
 };
