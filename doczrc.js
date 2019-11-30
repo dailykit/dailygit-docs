d49fb50f99941ffb052b318ba13fa63fa43c2e67
Praveen Bisht <prvnbist@gmail.com>
2019-11-30 21:22:53 +0530

    docs(page): create file

--- doczrc.js
+++ doczrc.js
@@ -1,9 +1,14 @@
 export default {
-  menu: ["Introduction"],
+  menu: [
+    "Introduction",
+    {
+      name: "File System",
+      menu: ["Create File"]
+    }
+  ],
   title: "DailyGit Docs",
   description: "Documentation for dailygit",
   public: "/public",
-  port: 3001,
   themeConfig: {
     colors: {
       header: {
@@ -12,6 +17,38 @@ export default {
           bg: "#6b9a25",
           color: "#fff"
         }
+      }
+    },
+    styles: {
+      Container: {
+        maxWidth: 980
+      },
+      table: {
+        width: "100%",
+        my: 4,
+        borderCollapse: "separate",
+        borderSpacing: 0,
+        [["th", "td"]]: {
+          textAlign: "left",
+          py: "4px",
+          pr: "4px",
+          pl: 0,
+          borderColor: "muted",
+          borderBottomStyle: "solid"
+        }
+      },
+      th: {
+        borderBottomWidth: 1,
+        borderBottomColor: "#e5eef7",
+        fontSize: 14,
+        textTransform: "uppercase",
+        color: "#9bacbd",
+        fontWeight: 500
+      },
+      td: {
+        verticalAlign: "top",
+        borderBottomWidth: "1px",
+        fontSize: 16
       }
     }
   }
